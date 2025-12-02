f2c71d869fa3f31e284f5a1f80161c65b546dcba
GrowthMeta Dev <dev@growthmeta.com>
2025-12-02 12:49:20 +0545

    Fix Sanity client syntax

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,13 +1,11 @@
-```javascript
 import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
-  projectId: projectId || 'placeholder',
-  dataset: dataset || 'production',
-  apiVersion,
-  useCdn,
-  perspective: 'published',
+    projectId: projectId || 'placeholder',
+    dataset: dataset || 'production',
+    apiVersion,
+    useCdn,
+    perspective: 'published',
 })
-```
