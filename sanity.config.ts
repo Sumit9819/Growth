62d8fad517294619c69fe4a00dea236464de34c5
GrowthMeta Dev <dev@growthmeta.com>
2025-12-02 12:52:56 +0545

    Add hardcoded fallback values to sanity.config for Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,28 +8,11 @@ import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { presentationTool } from 'sanity/presentation'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from './src/sanity/env'
-import { schema } from './src/sanity/schemaTypes'
-
-export default defineConfig({
-    basePath: '/studio',
-    projectId: projectId || '',
-    dataset: dataset || '',
-    // Add and edit the content schema in the './sanity/schema' folder
-    schema,
-    plugins: [
-        structureTool(),
-        // Vision is a tool that lets you query your content with GROQ in the studio
-        // https://www.sanity.io/docs/the-vision-plugin
-        visionTool({ defaultApiVersion: apiVersion }),
-        presentationTool({
-            previewUrl: {
-                draftMode: {
-                    enable: '/api/draft',
+previewUrl: {
+    draftMode: {
+        enable: '/api/draft',
                 },
-            },
+},
         }),
     ],
 })
