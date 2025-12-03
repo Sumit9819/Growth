ac012f7cd0b8a7f68418abf4484ae917446bd2b1
GrowthMeta Dev <dev@growthmeta.com>
2025-12-03 18:53:19 +0545

    fix: add fallback project id to prevent placeholder errors

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,5 +2,5 @@ export const apiVersion =
     process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-01'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '1dqts6rb';
 export const useCdn = false
--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
-    projectId: projectId || 'placeholder',
+    projectId,
     dataset: dataset || 'production',
     apiVersion,
     useCdn,
